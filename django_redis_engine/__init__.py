b42138dc87d00cba7783bfabf3389af3edc32795
Rylan Hazelton <rylan@badger.com>
2012-07-25 17:03:03 -0700

    update version

--- django_redis_engine/__init__.py
+++ django_redis_engine/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-__version__ = (0, 1, 3)
+__version__ = (0, 1, 4)
 __author__ = "Mirko Rossini (modified by Rylan Hazelton for OnSports)"
 __contact__ = "isaidyep@gmail.com"
 __docformat__ = "restructuredtext"
